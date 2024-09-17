de6c41263b161ac09aa635522d2579b09dfeed08
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-09-17 11:40:13 +0700

    code

---
+++ codelab2/bin/sheva.dart
@@ -0,0 +1,7 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main() {
+  for (int i = 18; i > 8; i--) {
+    print('hello nama saya vanda, sekarang saya berumur $i');
+  }
+}
